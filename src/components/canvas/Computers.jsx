ff8740f49d852de6a0290a9fc9f1283e681a9e25
Gargonta <51290161+Gargonta@users.noreply.github.com>
2023-05-23 17:07:51 +0300

    правка

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -29,7 +29,7 @@ const Computers = ({isMobile}) => {
       {/*/>*/}
         <primitive
           object={computer.scene}
-          scale={isMobile ? 0.6 : 0.75}
+          scale={isMobile ? 0.4 : 0.55}
           position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
           rotation={[-0.033, seconds, -0.1]}
         />
