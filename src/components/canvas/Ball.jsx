ae553baea46b9605babe99bb84eeac541bf017f2
Gargonta <51290161+Gargonta@users.noreply.github.com>
2023-05-23 17:22:22 +0300

    правка

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -17,24 +17,20 @@ const Ball = (props) => {
       <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
         <ambientLight intensity={0.25} />
         <directionalLight position={[0, 0, 0.05]} />
-        <mesh
-            // castShadow
-            // receiveShadow
-            radius={400}
-            scale={2.75}>
+        <mesh castShadow receiveShadow scale={2.75}>
           <icosahedronGeometry args={[1, 1]} />
           <meshStandardMaterial
               color='#fff8eb'
-              // polygonOffset
+              polygonOffset
               polygonOffsetFactor={-5}
-              // flatShading
+              flatShading
           />
           <Decal
               position={[0, 0, 1]}
               rotation={[2 * Math.PI, 0, 6.25]}
               scale={1}
               map={decal}
-              // flatShading
+              flatShading
           />
         </mesh>
       </Float>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -16,20 +16,20 @@ const Computers = ({isMobile}) => {
 
   return (
     <mesh>
-      {/*<hemisphereLight intensity={0.15}*/}
-      {/*                 groundColor="black" />*/}
+      <hemisphereLight intensity={0.15}
+                       groundColor="black" />
         <pointLight intensity={1}/>
-      {/*<spotLight*/}
-      {/*    position={[-20, 50, 10]}*/}
-      {/*    angle={0.12}*/}
-      {/*    penumbra={1}*/}
-      {/*    intensity={1}*/}
-      {/*    castShadow*/}
-      {/*    shadow-mapSize={512}*/}
-      {/*/>*/}
+      <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={512}
+      />
         <primitive
           object={computer.scene}
-          scale={isMobile ? 0.4 : 0.55}
+          scale={isMobile ? 0.6 : 0.75}
           position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
           rotation={[-0.033, seconds, -0.1]}
         />
@@ -41,7 +41,7 @@ const ComputersCanvas = () => {
     const [isMobile, setIsMobile] = useState(false);
     useEffect(() => {
         // Add a listener for changes to the screen size
-        const mediaQuery = window.matchMedia("(max-width: 450px)");
+        const mediaQuery = window.matchMedia("(max-width: 500px)");
 
         // Set the initial value of the `isMobile` state variable
         setIsMobile(mediaQuery.matches);
@@ -62,20 +62,18 @@ const ComputersCanvas = () => {
   return (
       <Canvas
           frameloop='demand'
-          // shadows
+          shadows
           dpr={[1, 2]}
           camera={{ position: [20, 3, 5], fov: 25 }}
-          // gl={{ preserveDrawingBuffer: true }}
+          gl={{ preserveDrawingBuffer: true }}
       >
         <Suspense fallback={<CanvasLoader />}>
-          {/*<OrbitControls*/}
-          {/*    enableZoom={false}*/}
-          {/*    maxPolarAngle={Math.PI / 2}*/}
-          {/*    minPolarAngle={Math.PI / 2}*/}
-          {/*/>*/}
-          <Computers
-              // isMobile={isMobile}
+          <OrbitControls
+              enableZoom={false}
+              maxPolarAngle={Math.PI / 2}
+              minPolarAngle={Math.PI / 2}
           />
+          <Computers isMobile={isMobile} />
         </Suspense>
 
         <Preload all />
